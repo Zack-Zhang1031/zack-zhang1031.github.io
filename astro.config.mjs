591053507d846e8ffefbae41800c872e78df047c
Administrator <kunzhang098@gmail.com>
2026-08-20 14:09:10 +0800

    [site] fix: update domain to zz1031.vercel.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://zack1031.vercel.app',
+  site: 'https://zz1031.vercel.app',
   output: 'static',
   adapter: vercel(),
   integrations: [tailwind(), sitemap()],
