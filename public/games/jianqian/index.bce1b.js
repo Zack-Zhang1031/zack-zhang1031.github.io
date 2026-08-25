6b741818f007c8aa3cfcd623fddd53cead538605
Administrator <kunzhang098@gmail.com>
2026-08-25 11:12:24 +0800

    [game] fix: stabilize Catch Money web startup

--- public/games/jianqian/index.bce1b.js
+++ public/games/jianqian/index.bce1b.js
@@ -1,4 +1,4 @@
-System.register(["./application.e92dc.js"], function (_export, _context) {
+System.register(["./application.45db3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
