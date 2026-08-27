4eadd8f0d1cb21d949640a34e6a8a326384ceece
Administrator <kunzhang098@gmail.com>
2026-08-27 10:41:32 +0800

    [games] update: publish latest bottle flip build

--- public/games/rengpingzi/index.859bd.js
+++ public/games/rengpingzi/index.859bd.js
@@ -1,4 +1,4 @@
-System.register(["./application.66a13.js"], function (_export, _context) {
+System.register(["./application.3f459.js"], function (_export, _context) {
   "use strict";
 
   var Application, application;
